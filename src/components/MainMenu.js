eb206417822996cce8dad34abcf170dd7faeb316
amohar <andrej.mohar@gmail.com>
2022-09-09 11:54:16 +0200

    Responsive: Menu is done

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -3,6 +3,7 @@ import { Link } from 'components/Router'
 import { Menu } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faBars } from '@fortawesome/pro-duotone-svg-icons';
 
 export function MainMenu() {
     const [current, setCurrent] = useState('product');
@@ -12,8 +13,12 @@ export function MainMenu() {
     };
     return (
       <>
-          <div className="logo"><img src="./img/NetScourSecLogo.png" /> NetScour Security</div>
-          <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" style={{marginLeft: "auto"}}>
+          <div className="logo"><img src="./img/NetScourSecLogo.png" />&nbsp;<span className="company-name">NetScour Security</span></div>
+          <Menu
+            onClick={onClick}
+            selectedKeys={[current]}
+            mode="horizontal"
+            overflowedIndicator={<FontAwesomeIcon icon={faBars} />}>
             <Menu.Item key="product">
               <Link to="/">Product</Link>
             </Menu.Item>
@@ -24,13 +29,13 @@ export function MainMenu() {
               <Link to="/preregister">Preregister</Link>
             </Menu.Item>
             <Menu.Item key="github" className="social">
-              <a href="#" target="_blank"><FontAwesomeIcon icon={faGithub} /></a>
+              <a href="#" target="_blank"><FontAwesomeIcon icon={faGithub} /><span class="social-name">Github</span></a>
             </Menu.Item>
             <Menu.Item key="twitter" className="social">
-              <a href="#" target="_blank"><FontAwesomeIcon icon={faTwitter} /></a>
+              <a href="#" target="_blank"><FontAwesomeIcon icon={faTwitter} /><span class="social-name">Twitter</span></a>
             </Menu.Item>
             <Menu.Item key="linkedin" className="social">
-              <a href="#" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a>
+              <a href="#" target="_blank"><FontAwesomeIcon icon={faLinkedin} /><span class="social-name">Linkedin</span></a>
             </Menu.Item>
           </Menu>
       </>
