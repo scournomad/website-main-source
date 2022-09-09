baf0c94076f5147018c8cfb9226ddb180d0942d9
amohar <andrej.mohar@gmail.com>
2022-09-09 18:15:51 +0200

    404 page done

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Button, Result } from 'antd';
 
 export default () => (
-  <div>
-    <h1>404 - Oh no's! We couldn't find that page :(</h1>
+  <div className="content">
+    <Result
+      status="404"
+      title="404 Not found"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={<Button type="primary" href='/'>Back Home</Button>} />
   </div>
 )
